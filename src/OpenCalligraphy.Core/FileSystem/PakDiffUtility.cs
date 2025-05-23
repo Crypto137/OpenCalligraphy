5acee86843c8e5936f3914df819deb1499cec4c3
Crypto137 <Crypto137@users.noreply.github.com>
2025-05-23 12:26:29 +0300

    Move PakFile diff logic to Core

---
+++ src/OpenCalligraphy.Core/FileSystem/PakDiffUtility.cs
@@ -0,0 +1,92 @@
+﻿using OpenCalligraphy.Core.Helpers;
+
+namespace OpenCalligraphy.Core.FileSystem
+{
+    /// <summary>
+    /// Compares <see cref="PakFile"/> instances.
+    /// </summary>
+    public static class PakDiffUtility
+    {
+        private const char PrefixAdded     = '+';
+        private const char PrefixRemoved   = '-';
+        private const char PrefixChanged   = '!';
+        private const char PrefixUnchanged = ' ';
+
+        /// <summary>
+        /// Loads <see cref="PakFile"/> instances from specified file paths and compares them. The result is written to the provided <see cref="TextWriter"/>.
+        /// </summary>
+        public static void Diff(string oldFilePath, string newFilePath, TextWriter outputWriter)
+        {
+            PakFile oldPakFile = new(oldFilePath);
+            PakFile newPakFile = new(newFilePath);
+
+            Diff(oldPakFile, newPakFile, outputWriter);
+        }
+
+        /// <summary>
+        /// Compares provided <see cref="PakFile"/> instances. The result is written to the provided <see cref="TextWriter"/>.
+        /// </summary>
+        public static void Diff(PakFile oldPakFile, PakFile newPakFile, TextWriter outputWriter)
+        {
+            ArgumentNullException.ThrowIfNull(oldPakFile);
+            ArgumentNullException.ThrowIfNull(newPakFile);
+            ArgumentNullException.ThrowIfNull(outputWriter);
+
+            CalculateFileChecksums(oldPakFile, out Dictionary<string, uint> oldChecksums);
+            CalculateFileChecksums(newPakFile, out Dictionary<string, uint> newChecksums);
+
+            SortedSet<string> fileSet = new();
+            fileSet.UnionWith(oldChecksums.Keys);
+            fileSet.UnionWith(newChecksums.Keys);
+
+            foreach (string fileName in fileSet)
+            {
+                char prefix = GetFilePrefix(fileName, oldChecksums, newChecksums);
+                if (prefix == PrefixUnchanged)
+                    continue;
+
+                outputWriter.WriteLine($"{prefix} {fileName}");
+            }
+        }
+
+        /// <summary>
+        /// Calculates checksums for all files in the provided <see cref="PakFile"/> and adds them to the provided <see cref="Dictionary{TKey, TValue}"/>.
+        /// </summary>
+        private static void CalculateFileChecksums(PakFile pakFile, out Dictionary<string, uint> checksumDict)
+        {
+            checksumDict = new();
+
+            foreach (PakFile.Entry entry in pakFile)
+            {
+                Stream stream = pakFile.LoadFileDataInPak(entry.FilePath);
+                checksumDict.Add(entry.FilePath, HashHelper.Djb2(stream));
+            }
+        }
+
+        /// <summary>
+        /// Determines prefix for the specified file name based on the provided checksum collections.
+        /// </summary>
+        private static char GetFilePrefix(string fileName, Dictionary<string, uint> oldChecksums, Dictionary<string, uint> newChecksums)
+        {
+            bool wasPresentBefore = oldChecksums.TryGetValue(fileName, out uint oldChecksum);
+            bool isPresentNow = newChecksums.TryGetValue(fileName, out uint newChecksum);
+
+            if (wasPresentBefore && isPresentNow)
+            {
+                if (oldChecksum != newChecksum)
+                    return PrefixChanged;
+                else
+                    return PrefixUnchanged;
+            }
+            else
+            {
+                if (wasPresentBefore)
+                    return PrefixRemoved;
+                else if (isPresentNow)
+                    return PrefixAdded;
+            }
+
+            throw new($"Failed to determine prefix for file '{fileName}'.");   // This should never happen
+        }
+    }
+}
--- src/OpenCalligraphy.DiffTool/Program.cs
+++ src/OpenCalligraphy.DiffTool/Program.cs
@@ -1,17 +1,9 @@
-﻿using OpenCalligraphy.Core.Exceptions;
-using OpenCalligraphy.Core.FileSystem;
-using OpenCalligraphy.Core.Helpers;
+﻿using OpenCalligraphy.Core.FileSystem;
 
 namespace OpenCalligraphy.DiffTool
 {
     internal class Program
     {
-        const char PrefixAdded      = '+';
-        const char PrefixRemoved    = '-';
-        const char PrefixChanged    = '!';
-        const char PrefixUnchanged  = ' ';
-        const char PrefixUnknown    = '?';
-
         static void Main(string[] args)
         {
             if (args.Length < 2)
@@ -20,95 +12,14 @@ namespace OpenCalligraphy.DiffTool
                 return;
             }
 
-            if (LoadPakFile(args[0], out PakFile oldPakFile) == false)
-                return;
-
-            if (LoadPakFile(args[1], out PakFile newPakFile) == false)
-                return;
-
-            CalculateFileChecksums(oldPakFile, out Dictionary<string, uint> oldChecksums);
-            CalculateFileChecksums(newPakFile, out Dictionary<string, uint> newChecksums);
-
-            SortedSet<string> fileSet = new();
-            fileSet.UnionWith(oldChecksums.Keys);
-            fileSet.UnionWith(newChecksums.Keys);
-
-            foreach (string fileName in fileSet)
-            {
-                char prefix = GetFilePrefix(fileName, oldChecksums, newChecksums);
-                if (prefix == PrefixUnchanged)
-                    continue;
-
-                Console.WriteLine($"{prefix} {fileName}");
-            }
-        }
-
-        static bool LoadPakFile(string path, out PakFile pakFile)
-        {
-            pakFile = null;
-
-            if (File.Exists(path) == false)
-            {
-                Console.WriteLine($"'{path}' not found.");
-                return false;
-            }
-
             try
             {
-                pakFile = new(path);
+                PakDiffUtility.Diff(args[0], args[1], Console.Out);
             }
-            catch (CalligraphyException calligraphyException)
+            catch (Exception e)
             {
-                Console.WriteLine($"Failed to load {path}: '{calligraphyException.Message}'");
-                return false;
+                Console.WriteLine(e.ToString());
             }
-            catch (Exception exception)
-            {
-                Console.WriteLine($"Failed to load {path}:\n{exception}");
-                return false;
-            }
-
-            return true;
-        }
-
-        static void CalculateFileChecksums(PakFile pakFile, out Dictionary<string, uint> checksumDict)
-        {
-            checksumDict = new();
-
-            foreach (PakFile.Entry entry in pakFile)
-            {
-                Stream stream = pakFile.LoadFileDataInPak(entry.FilePath);
-                checksumDict.Add(entry.FilePath, HashHelper.Djb2(stream));
-            }
-        }
-
-        static char GetFilePrefix(string fileName, Dictionary<string, uint> oldChecksums, Dictionary<string, uint> newChecksums)
-        {
-            bool wasPresentBefore = oldChecksums.TryGetValue(fileName, out uint oldChecksum);
-            bool isPresentNow = newChecksums.TryGetValue(fileName, out uint newChecksum);
-
-            if (wasPresentBefore && isPresentNow)
-            {
-                if (oldChecksum != newChecksum)
-                    return PrefixChanged;
-                else
-                    return PrefixUnchanged;
-            }
-            else
-            {
-                if (wasPresentBefore)
-                    return PrefixRemoved;
-                else if (isPresentNow)
-                    return PrefixAdded;
-            }
-
-            return PrefixUnknown;   // This shouldn't happen
-        }
-
-        readonly struct FileInfo(string name, uint checksum)
-        {
-            public readonly string Name = name;
-            public readonly uint Checksum = checksum;
         }
     }
 }
