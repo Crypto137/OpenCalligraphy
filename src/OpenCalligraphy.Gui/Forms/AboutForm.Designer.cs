90295f9e2a574ef128ad23860fcf45ba953cc8d1
Crypto137 <Crypto137@users.noreply.github.com>
2025-05-27 14:55:29 +0300

    Adjust AboutForm layout

--- src/OpenCalligraphy.Gui/Forms/AboutForm.Designer.cs
+++ src/OpenCalligraphy.Gui/Forms/AboutForm.Designer.cs
@@ -30,58 +30,64 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AboutForm));
             aboutPanel = new Panel();
+            aboutGroupBox = new GroupBox();
             aboutTableLayoutPanel = new TableLayoutPanel();
-            iconPictureBox = new PictureBox();
             infoTableLayoutPanel = new TableLayoutPanel();
             titleLabel = new Label();
-            copyrightLabel = new Label();
-            websiteLinkLabel = new LinkLabel();
             versionLabel = new Label();
+            websiteLinkLabel = new LinkLabel();
+            copyrightLabel = new Label();
             licenseLabel = new Label();
+            iconPictureBox = new PictureBox();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            okButton = new Button();
             aboutPanel.SuspendLayout();
+            aboutGroupBox.SuspendLayout();
             aboutTableLayoutPanel.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)iconPictureBox).BeginInit();
             infoTableLayoutPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)iconPictureBox).BeginInit();
+            tableLayoutPanel1.SuspendLayout();
             SuspendLayout();
             // 
             // aboutPanel
             // 
-            aboutPanel.Controls.Add(aboutTableLayoutPanel);
+            aboutPanel.Controls.Add(aboutGroupBox);
+            aboutPanel.Controls.Add(tableLayoutPanel1);
             aboutPanel.Dock = DockStyle.Fill;
             aboutPanel.Location = new Point(0, 0);
             aboutPanel.Name = "aboutPanel";
             aboutPanel.Padding = new Padding(8);
-            aboutPanel.Size = new Size(464, 141);
+            aboutPanel.Size = new Size(464, 191);
             aboutPanel.TabIndex = 0;
+            // 
+            // aboutGroupBox
+            // 
+            aboutGroupBox.Controls.Add(aboutTableLayoutPanel);
+            aboutGroupBox.Dock = DockStyle.Fill;
+            aboutGroupBox.Location = new Point(8, 8);
+            aboutGroupBox.Name = "aboutGroupBox";
+            aboutGroupBox.Size = new Size(448, 146);
+            aboutGroupBox.TabIndex = 8;
+            aboutGroupBox.TabStop = false;
+            aboutGroupBox.Text = "About";
             // 
             // aboutTableLayoutPanel
             // 
             aboutTableLayoutPanel.ColumnCount = 2;
             aboutTableLayoutPanel.ColumnStyles.Add(new ColumnStyle());
             aboutTableLayoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
-            aboutTableLayoutPanel.Controls.Add(iconPictureBox, 0, 0);
             aboutTableLayoutPanel.Controls.Add(infoTableLayoutPanel, 1, 0);
+            aboutTableLayoutPanel.Controls.Add(iconPictureBox, 0, 0);
             aboutTableLayoutPanel.Dock = DockStyle.Fill;
-            aboutTableLayoutPanel.Location = new Point(8, 8);
+            aboutTableLayoutPanel.Location = new Point(3, 19);
             aboutTableLayoutPanel.Name = "aboutTableLayoutPanel";
             aboutTableLayoutPanel.RowCount = 1;
             aboutTableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
-            aboutTableLayoutPanel.Size = new Size(448, 125);
+            aboutTableLayoutPanel.Size = new Size(442, 124);
             aboutTableLayoutPanel.TabIndex = 6;
-            // 
-            // iconPictureBox
-            // 
-            iconPictureBox.Image = (Image)resources.GetObject("iconPictureBox.Image");
-            iconPictureBox.Location = new Point(3, 3);
-            iconPictureBox.Name = "iconPictureBox";
-            iconPictureBox.Size = new Size(128, 119);
-            iconPictureBox.SizeMode = PictureBoxSizeMode.Zoom;
-            iconPictureBox.TabIndex = 5;
-            iconPictureBox.TabStop = false;
             // 
             // infoTableLayoutPanel
             // 
-            infoTableLayoutPanel.Anchor = AnchorStyles.Left;
             infoTableLayoutPanel.ColumnCount = 1;
             infoTableLayoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
             infoTableLayoutPanel.Controls.Add(titleLabel, 0, 0);
@@ -89,6 +95,7 @@
             infoTableLayoutPanel.Controls.Add(websiteLinkLabel, 0, 4);
             infoTableLayoutPanel.Controls.Add(copyrightLabel, 0, 2);
             infoTableLayoutPanel.Controls.Add(licenseLabel, 0, 3);
+            infoTableLayoutPanel.Dock = DockStyle.Fill;
             infoTableLayoutPanel.Location = new Point(137, 3);
             infoTableLayoutPanel.Name = "infoTableLayoutPanel";
             infoTableLayoutPanel.RowCount = 5;
@@ -97,7 +104,7 @@
             infoTableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
             infoTableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
             infoTableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
-            infoTableLayoutPanel.Size = new Size(308, 119);
+            infoTableLayoutPanel.Size = new Size(302, 118);
             infoTableLayoutPanel.TabIndex = 6;
             // 
             // titleLabel
@@ -110,28 +117,6 @@
             titleLabel.TabIndex = 1;
             titleLabel.Text = "OpenCalligraphy";
             // 
-            // copyrightLabel
-            // 
-            copyrightLabel.Anchor = AnchorStyles.Left;
-            copyrightLabel.AutoSize = true;
-            copyrightLabel.Location = new Point(3, 42);
-            copyrightLabel.Name = "copyrightLabel";
-            copyrightLabel.Size = new Size(158, 15);
-            copyrightLabel.TabIndex = 3;
-            copyrightLabel.Text = "Copyright © 2025 Crypto137";
-            // 
-            // websiteLinkLabel
-            // 
-            websiteLinkLabel.Anchor = AnchorStyles.Left;
-            websiteLinkLabel.AutoSize = true;
-            websiteLinkLabel.Location = new Point(3, 101);
-            websiteLinkLabel.Name = "websiteLinkLabel";
-            websiteLinkLabel.Size = new Size(262, 15);
-            websiteLinkLabel.TabIndex = 4;
-            websiteLinkLabel.TabStop = true;
-            websiteLinkLabel.Text = "https://github.com/Crypto137/OpenCalligraphy";
-            websiteLinkLabel.LinkClicked += websiteLinkLabel_LinkClicked;
-            // 
             // versionLabel
             // 
             versionLabel.Anchor = AnchorStyles.Left;
@@ -141,6 +126,28 @@
             versionLabel.Size = new Size(118, 15);
             versionLabel.TabIndex = 2;
             versionLabel.Text = "Version 0.0.0 (Debug)";
+            // 
+            // websiteLinkLabel
+            // 
+            websiteLinkLabel.Anchor = AnchorStyles.Left;
+            websiteLinkLabel.AutoSize = true;
+            websiteLinkLabel.Location = new Point(3, 100);
+            websiteLinkLabel.Name = "websiteLinkLabel";
+            websiteLinkLabel.Size = new Size(262, 15);
+            websiteLinkLabel.TabIndex = 4;
+            websiteLinkLabel.TabStop = true;
+            websiteLinkLabel.Text = "https://github.com/Crypto137/OpenCalligraphy";
+            websiteLinkLabel.LinkClicked += websiteLinkLabel_LinkClicked;
+            // 
+            // copyrightLabel
+            // 
+            copyrightLabel.Anchor = AnchorStyles.Left;
+            copyrightLabel.AutoSize = true;
+            copyrightLabel.Location = new Point(3, 42);
+            copyrightLabel.Name = "copyrightLabel";
+            copyrightLabel.Size = new Size(158, 15);
+            copyrightLabel.TabIndex = 3;
+            copyrightLabel.Text = "Copyright © 2025 Crypto137";
             // 
             // licenseLabel
             // 
@@ -152,11 +159,46 @@
             licenseLabel.TabIndex = 5;
             licenseLabel.Text = "This software is licensed under the MIT License.\r\nSee the included LICENSE.txt file for more information.";
             // 
+            // iconPictureBox
+            // 
+            iconPictureBox.Image = (Image)resources.GetObject("iconPictureBox.Image");
+            iconPictureBox.Location = new Point(3, 3);
+            iconPictureBox.Name = "iconPictureBox";
+            iconPictureBox.Size = new Size(128, 118);
+            iconPictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+            iconPictureBox.TabIndex = 5;
+            iconPictureBox.TabStop = false;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.AutoSize = true;
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 80F));
+            tableLayoutPanel1.Controls.Add(okButton, 1, 0);
+            tableLayoutPanel1.Dock = DockStyle.Bottom;
+            tableLayoutPanel1.Location = new Point(8, 154);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 1;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.Size = new Size(448, 29);
+            tableLayoutPanel1.TabIndex = 7;
+            // 
+            // okButton
+            // 
+            okButton.Location = new Point(371, 3);
+            okButton.Name = "okButton";
+            okButton.Size = new Size(74, 23);
+            okButton.TabIndex = 0;
+            okButton.Text = "OK";
+            okButton.UseVisualStyleBackColor = true;
+            // 
             // AboutForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(464, 141);
+            CancelButton = okButton;
+            ClientSize = new Size(464, 191);
             Controls.Add(aboutPanel);
             FormBorderStyle = FormBorderStyle.FixedSingle;
             MaximizeBox = false;
@@ -166,10 +208,13 @@
             StartPosition = FormStartPosition.CenterParent;
             Text = "About OpenCalligraphy";
             aboutPanel.ResumeLayout(false);
+            aboutPanel.PerformLayout();
+            aboutGroupBox.ResumeLayout(false);
             aboutTableLayoutPanel.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)iconPictureBox).EndInit();
             infoTableLayoutPanel.ResumeLayout(false);
             infoTableLayoutPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)iconPictureBox).EndInit();
+            tableLayoutPanel1.ResumeLayout(false);
             ResumeLayout(false);
         }
 
@@ -184,5 +229,8 @@
         private TableLayoutPanel aboutTableLayoutPanel;
         private TableLayoutPanel infoTableLayoutPanel;
         private Label licenseLabel;
+        private TableLayoutPanel tableLayoutPanel1;
+        private Button okButton;
+        private GroupBox aboutGroupBox;
     }
 }
